466f1af60f14a1d0dbe7f0efc1264fd7e22bea3b
danielnoukas <daniel.noukas@gmail.com>
2020-01-27 11:32:56 +0200

    asdas

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const btns = Array.prototype.slice.call(document.querySelectorAll(".btn"))
 const answer = document.querySelector('#answer');
+let currentnumber = '';
+let firstnumber;
 
 btns.map(btn => btn.addEventListener('click',(evt) => {
-    answer.innerText = evt.target.innerText
-
+    if (typeof parseInt(evt.target.innerText) == 'number') {
+        saveCurrentNumber(evt.target.innertext)
+    }
 }))
+
+function saveCurrentNumber(number) {
+    currentnumber += number
+    updateDisplay();
+}
+
+function updateDisplay() {
+    answer.innerText = currentNumber
+}
